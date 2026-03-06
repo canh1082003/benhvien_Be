51bb44cea8dbca060b6adde24eb2531faee54af2
Nguyen Cong Anh <nguyenconganh@MacBook-Air-cua-Nguyen-3.local>
2026-03-06 10:55:38 +0700

    Fix build.sh newline issue

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migratepython seed_data.py
+python manage.py migrate
+python seed_data.py
